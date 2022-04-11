d444b17b726903415b0527802ee58bc65dc39716
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-04-12 06:48:28 +0700

    Menggunakan useEffect untuk efektivitas fetching data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,76 @@
-import React from 'react'
-import Card from './Components/Card';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [user, setUser] = useState([]);
+  const [identifier, setIdentifier] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const getUser = async () => {
+    setLoading(true);
+    try {
+      let response = await axios.get(`https://jsonplaceholder.typicode.com/users/${identifier}`);
+      setUser(response.data);
+      setLoading(false);
+    } catch(e) {
+      setLoading(true);
+      console.log(e.message);
+    }
+  } 
+
+  useEffect(() => {
+    getUser()
+  }, [ identifier ])
+
   return (
-    <div className="py-4">
+    <div className="py-5">
       <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <Card content={{
-              imageUrl: "https://placekitten.com/g/300/200",
-              title: "The first post",
-              published: "12 April 2022",
-            }}  
-            />
-          </div>
-          <div className="col-md-4">
-            <Card content={{
-              imageUrl: "https://placekitten.com/300/200",
-              title: "The second post",
-              published: "11 April 2022",
-            }} 
-            />
-          </div>
-          <div className="col-md-4">
-            <Card content={{
-              imageUrl: "https://placekitten.com/g/300/200",
-              title: "The third post",
-              published: "08 April 2022",   
-            }} 
-            />
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <div className="my-3">
+              <input type="text" value={identifier} onChange={(e) => setIdentifier(e.target.value)} name="identifier" id="identifier" />
+            </div>
+            {
+              loading ? <div>Loading . . .</div> : 
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Website</th>
+                    <th>Phone number</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                      <td>{user.name}</td>
+                      <td>{user.username}</td>
+                      <td>{user.email}</td>
+                      <td>{user.website}</td>
+                      <td>{user.phone}</td>
+                    </tr>
+                  {/* {
+                    users.map((user, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>{user.name}</td>
+                          <td>{user.username}</td>
+                          <td>{user.email}</td>
+                          <td>{user.website}</td>
+                          <td>{user.phone}</td>
+                        </tr>
+                      )
+                    })
+                  } */}
+                </tbody>
+              </table>
+            }
           </div>
         </div>
       </div>
     </div>
   )
-
 }
 
 export default App;
