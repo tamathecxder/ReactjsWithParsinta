ee58b83ad0d5bb681f289d5b57e233baa9e620b0
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-04-12 00:54:25 +0700

    Implementasi studycase statefull menjadi stateless

--- src/App.js
+++ src/App.js
@@ -1,65 +1,48 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class App extends Component {
-  constructor() {
-    super()
+function App() {
+  const [ firstname, setFirstname ] = useState('');
+  const [ lastname, setLastname ] = useState('');
+  const [ name, setName ] = useState('');
 
-    this.state = {
-      firstname: '',
-      lastname: '',
-      name: ''
-    };
-
-  }
-
-  changeHandler = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-
-  submitHandler = (e) => {
+  const submitHandler = (e) => {
     e.preventDefault();
-    this.setState({
-      name: `${this.state.firstname} ${this.state.lastname}`
-    });
-    
-    this.state.firstname = ''
+    setName(`${firstname} ${lastname}`);
+    setFirstname('')
+    setLastname('')
   }
-  
-  render() { 
-    const {name} = this.state;
-    return(
-      <div className="p-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <div className="card">
-                <div className="card-header">Learn React JS</div>
-                <div className="card-body">
-                  <form onSubmit={this.submitHandler}>
-                    <div className="mb-4">
-                      <label htmlFor="firstname" className="form-label">Firstname</label>
-                      <input type="text" onChange={this.changeHandler} value={this.state.firstname} name="firstname" id="firstname" className="form-control" />
-                    </div>
-                    <div className="mb-4">
-                      <label htmlFor="lastname" className="form-label">lastname</label>
-                      <input type="text" onChange={this.changeHandler} value={this.state.lastname} name="lastname" id="lastname" className="form-control" />
-                    </div>
-                    <button type="submit" className="btn btn-primary btn-block">Show</button>
-                  </form>
-                </div>
-                <div className="card-footer">
-                  My name is {name ? name : '. . .'}
-                </div>
+
+  return (
+    <div className="p-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5">
+            <div className="card">
+              <div className="card-header">
+                Learn React Function
+              </div>
+              <div className="card-body">
+                <form onSubmit={submitHandler}>
+                  <div className="mb-4">
+                    <label htmlFor="firstname" className="form-label">Firstname</label>
+                    <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} name="firstname" id="firstname" className="form-control" />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="lastname" className="form-label">Lastname</label>
+                    <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} name="lastname" id="lastname" className="form-control" />
+                  </div>
+                  <button type="submit" className="btn btn-primary btn-block">Show</button>
+                </form>
+              </div>
+              <div className="card-footer">
+                My name is {name ? name : '...'}
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
-  }
-
+    </div>
+  )
 }
 
 export default App;
