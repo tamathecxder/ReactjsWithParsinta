975649ab9aa5192cb47858e0282c252f42bd9144
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-04-11 22:58:04 +0700

    Mencoba malakukan fetching data API dengan axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
- import React, { Component } from "react";
+import React, { Component } from "react";
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      user: []
+    }
+
+  }
+
+  getUser = async () => {
+    let response = await axios('https://jsonplaceholder.typicode.com/users/1');
+    this.setState({
+      user: response.data
+    });
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+  
   render() {
+    const user = this.state.user;
     return(
       <div>
-        <div>Hello World</div>
-        <div>Hello World</div>
+        <div>My name is {user.name}</div>
+        <div>My name is {user.username}</div>
+        <div>My name is {user.address.city}</div>
       </div>
     )
   }
