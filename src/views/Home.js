fab60aa9129b20faaa0e7588ccec9f5880de6cc2
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-04-14 17:31:52 +0700

    Mencoba react contect

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext, UserProvider } from './context/User';
+
+const User = () => { 
+  const {user} = useContext(UserContext);
+  return (
+    <div>
+      My name is {user.name}
+    </div>
+  ) 
+}
 
 function Home(props) {
   return (
     <div className="container">
-      Hello World
+      <UserProvider>
+        <User/>
+      </UserProvider>
     </div>
   );
 }
